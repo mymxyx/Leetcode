2e51d24a9ebf1ffaf83eb39176b6870046ce14c0
mymxyx <yixiuhuang1225@gmail.com>
2016-11-07 13:52:34 -0500

    Create 350 Intersection of Two Arrays II.java

---
+++ 350 Intersection of Two Arrays II.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i =0,j=0;
+        ArrayList<Integer> list = new ArrayList<>();
+        while(i<nums1.length & j<nums2.length)
+        {
+            if(nums1[i] == nums2[j])
+            {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j])
+            {
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        
+        int size = list.size();
+        int[] ret = new int[size];
+        int index = 0;
+        for(Integer num: list)
+        {
+            ret[index++] = num;
+        }
+        return ret;
+    }
+}
