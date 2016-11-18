8ab41829036f04cf9fcf4554d4c8b5b2320aa782
mymxyx <yixiuhuang1225@gmail.com>
2016-11-18 11:54:50 -0500

    Update 374 Guess Number Higher or Lower.java

--- 374 Guess Number Higher or Lower.java
+++ 374 Guess Number Higher or Lower.java
@@ -26,3 +26,34 @@ public class Solution extends GuessGame {
     }
     
 }
+
+// first version! Accepted!
+/* The guess API is defined in the parent class GuessGame.
+   @param num, your guess
+   @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+      int guess(int num); */
+/*  
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        return solution(1,n); 
+    }
+    
+    private int solution(int l, int r){
+        int pivot = l+(r-l)/2;
+		int result = guess(pivot);
+		if(result == -1) {
+			pivot = solution(l,pivot-1);
+		}
+		if(result == 1) {
+			pivot = solution(pivot+1,r);
+		}
+		if(result == 0){
+		    return pivot;
+		}
+		return pivot;
+        
+		
+	}
+}
+
+*/
