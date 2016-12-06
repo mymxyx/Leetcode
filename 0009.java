0f0d578ea452a0f83ff873fae8f32ac8a4e80159
mymxyx <yixiuhuang1225@gmail.com>
2016-12-06 09:13:16 -0500

    Update 0009.java

--- 0009.java
+++ 0009.java
@@ -31,6 +31,7 @@ public class MST {
          citys.add(new Connection("D","F",4));
          citys.add(new Connection("D","E",5));
          citys.add(new Connection("E","F",2));
+         
          ArrayList<Connection> result = solution(citys);
          for(Connection c: result){
         	 c.printConnection();
@@ -38,6 +39,55 @@ public class MST {
 	}
 	
 	public static ArrayList<Connection> solution(ArrayList<Connection> citys){
+		
+		ArrayList<Connection> ret = new ArrayList<Connection>();
+		
+		if(citys ==null || citys.size()==0){
+			return ret;
+		}
+		
+		ArrayList<String> visitedCity = new ArrayList<String>();
+		
+		Comparator<Connection> cmp = new Comparator<Connection>(){
+			public int compare(Connection o1,Connection o2){
+				if(o1.cost>o2.cost){
+					return 1;
+				}
+				else if(o1.cost<o2.cost){
+					return -1;
+				}
+				else{
+					return 0;
+				}
+			}
+		};
+		Collections.sort(citys,cmp);
+		
+		while(!citys.isEmpty()){
+			Connection tmp = helper(citys,visitedCity);
+			if(tmp == null){
+				break;
+			}
+			if(tmp!= null){
+				citys.remove(tmp);
+				visitedCity.add(tmp.city1);
+				visitedCity.add(tmp.city2);
+				ret.add(tmp);
+			}
+		}
+		Comparator<Connection> cmp2 = new Comparator<Connection>(){
+			public int compare(Connection o1, Connection o2){
+				if(o1.city1.equals(o2.city1)){
+					return o1.city2.compareTo(o2.city2);
+				}
+				else{
+					return o1.city1.compareTo(o2.city1);
+				}
+			}
+		};
+		Collections.sort(ret,cmp2);
+		return ret;
+/*		
 		ArrayList<Connection> ret = new ArrayList<Connection>();
 		if(citys.size()==0 || citys == null){
 			return ret;
@@ -71,21 +121,21 @@ public class MST {
 		
 		
 		return ret;
+*/
 	}
 	
-	public static Connection findmin(ArrayList<Connection> citys,ArrayList<String> visited){
-		int min = Integer.MAX_VALUE;
+	public static Connection helper(ArrayList<Connection> citys,ArrayList<String> visited){
 		Connection ret = null;
-		for(Connection c : citys){
-			if(c.cost<=min){
-				if((visited.contains(c.city1) && !visited.contains(c.city2)) || (visited.contains(c.city2)&& !visited.contains(c.city1))){
-					min = c.cost;
-					ret = c;
-				}
-				if(visited.isEmpty()){
-					min = c.cost;
-					ret = c;
-				}
+		int min = Integer.MAX_VALUE;
+		for(Connection c: citys){
+			if(c.cost<min){
+			if((visited.contains(c.city1) && !visited.contains(c.city2))||(visited.contains(c.city2)&& !(visited.contains(c.city1)))){
+				min = c.cost;
+				ret = c;
+			}
+			if(visited.isEmpty()){
+				ret = c;
+			}
 			}
 		}
 		return ret;
