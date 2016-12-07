36060b7ef6a980d9eb57fa72828bcc57330a902c
mymxyx <yixiuhuang1225@gmail.com>
2016-12-07 15:24:27 -0500

    Create 110 Balanced Binary Tree.java

---
+++ 110 Balanced Binary Tree.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        if(Math.abs(helper(root.left)-helper(root.right))>1){
+            return false;
+        }
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int helper(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        else{
+           return 1+Math.max(helper(root.left),helper(root.right)); 
+        } 
+    }
+}
