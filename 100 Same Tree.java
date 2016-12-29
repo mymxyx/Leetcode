0a75f5dfb71dd330775c827a53a8c540f98f6874
Yixiu Huang <yixiuhuang1225@gmail.com>
2016-12-29 11:50:22 -0500

    Update 100 Same Tree.java
    Two binary trees are considered the same if they have identical structure and nodes have the same value. 
    This problem can be solved using a simple recursive function.

--- 100 Same Tree.java
+++ 100 Same Tree.java
@@ -6,7 +6,7 @@
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
- */
+ 
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null){
@@ -40,4 +40,39 @@ public class Solution {
         }
         return a&&b&&c;
     }
+}
+ 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        //equal nullity donates that this branch is the same
+        //this is the base case but also handles being given two empty tree
+        if(p == null && q == null){
+            return true;
+        }
+        //unequal nullity donates that this branch aren't the same
+        //noetes that we have already ruled out equal nullity above
+        else if(p == null || q == null){
+            return false;
+        }
+        //if both nodes have values, check if this two nodes are equal
+        // decend to the next layer of the tree, "&&" here allows for any difference to overrule a local equality
+        if(p.val == q.val){
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        }
+        //if we are here, both nodes have values, and they are unequal, so the trees aren't the same.
+        else{
+            return false;
+        }
+    }
+
 }
