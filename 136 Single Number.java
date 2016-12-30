90677a9330dd6b8d4fe9f602ae468b2f069e2eeb
Yixiu Huang <yixiuhuang1225@gmail.com>
2016-12-30 15:55:20 -0500

    Update 136 Single Number.java

--- 136 Single Number.java
+++ 136 Single Number.java
@@ -1,5 +1,20 @@
 public class Solution {
     public int singleNumber(int[] nums) {
+        if(nums.length == 0) return -1;
+        if(nums.length == 1) return nums[0];
+        Arrays.sort(nums);
+        for(int i = 0;i<nums.length-1;i=i+2){
+            if(nums[i]!= nums[i+1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
+
+/*
+public class Solution {
+    public int singleNumber(int[] nums) {
         HashSet<Integer> hashset = new HashSet<Integer>();
 		for(int i =0;i<nums.length;i++){
 			if(!hashset.contains(nums[i])){
@@ -13,3 +28,4 @@ public class Solution {
         return itr.next();
     }
 }
+*/
